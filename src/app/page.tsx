1d2ae76397094902763bff0ade8575e67fa6cdab
CreativeCenterJapan <creative.center.japan@gmail.com>
2025-05-26 22:01:11 +0900

    Renew

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -478,7 +478,6 @@ export default function Home() {
                     {(() => {
                       const item = CHECK_ITEMS.find(i => i.label === showDetail);
                       const logsForItem = status.filter(log => log.includes(item?.keyword || ''));
-                      const isOK = item ? checkIsOK(item, logsForItem) : false;
 
                       return null;
                     })()}
